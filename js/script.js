c6f51db2cc81f7e7a272a56970c7ece5240ca655
Anton <anton.gayvoronskyy@googlemail.com>
2023-03-29 12:15:14 +0300

    add Local Storage

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 const taskForm = document.querySelector(".task__form"),
   taskInput = document.querySelector(".task__input"),
   taskButton = document.querySelector(".task__btn"),
-  taskList = document.querySelector(".task__list"),
-  taskItem = document.querySelector(".task__item"),
-  doneButton = document.querySelector(".done__icon");
+  taskList = document.querySelector(".task__list");
 
 let taskArr = [];
+
+if (localStorage.getItem("ToDo")) {
+  let storageArr = JSON.parse(localStorage.getItem("ToDo"));
+  storageArr.forEach((elem) => {
+    const taskDone = elem.done ? "task__item done" : "task__item";
+
+    const newBlock = `<li id='${elem.id}' class="${taskDone}">${elem.task}
+      <img src="./img/delete.svg" alt="" class="delete__icon icon">
+      <img src="./img/done.svg" alt="" class="done__icon icon">
+      <hr class="task__line"/>
+      </li>`;
+    taskList.insertAdjacentHTML("beforeend", newBlock);
+  });
+  taskArr = storageArr;
+  saveToLocalStorage();
+}
 
 taskButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -13,33 +27,12 @@ taskButton.addEventListener("click", (e) => {
 });
 
 taskList.addEventListener("click", (e) => {
-  if (e.target.classList.contains("done__icon")) {
-    const parentNode = e.target.closest(".task__item");
-    parentNode.classList.toggle("done");
-
-    localStorage.setItem("ToDo", JSON.stringify(taskArr));
-
-    taskArr.find((elem) => {
-      if (parentNode.id == elem.id) {
-        elem.done = !elem.done;
-      }
-    });
-  }
+  doneTask(e);
 });
 
 taskList.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete__icon")) {
-    const parentNode = e.target.closest(".task__item");
-    taskArr = taskArr.filter((elem) => parentNode.id != elem.id);
-    parentNode.remove();
-    localStorage.setItem("ToDo", JSON.stringify(taskArr));
-  }
+  deleteTask(e);
 });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   console;
-//   getSrorage();
-// });
 
 function addTask() {
   if (taskInput.value == "") return;
@@ -62,16 +55,34 @@ function addTask() {
       </li>`;
   taskList.insertAdjacentHTML("beforeend", newBlock);
 
-  localStorage.setItem("ToDo", JSON.stringify(taskArr));
+  saveToLocalStorage();
 
   taskInput.value = "";
 }
 
-// function getSrorage() {
-//   const Arr = localStorage.getItem("ToDo");
-//   console.log(JSON.parse(Arr));
-//   const taskArrStorage = [];
-//   if (JSON.parse(Arr) != null) {
-//     taskArrStorage = JSON.parse(Arr);
-//   }
-// }
+function doneTask(e) {
+  if (e.target.classList.contains("done__icon")) {
+    const parentNode = e.target.closest(".task__item");
+    parentNode.classList.toggle("done");
+
+    taskArr.find((elem) => {
+      if (parentNode.id == elem.id) {
+        elem.done = !elem.done;
+      }
+    });
+  }
+  saveToLocalStorage();
+}
+
+function deleteTask(e) {
+  if (e.target.classList.contains("delete__icon")) {
+    const parentNode = e.target.closest(".task__item");
+    taskArr = taskArr.filter((elem) => parentNode.id != elem.id);
+    parentNode.remove();
+  }
+  saveToLocalStorage();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("ToDo", JSON.stringify(taskArr));
+}
